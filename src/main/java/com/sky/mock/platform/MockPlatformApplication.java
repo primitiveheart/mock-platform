ccf212eb5208a58d21b9661bf8130cb43234475d
周官宝 <zhouguanbao@192.168.1.2>
2023-07-10 21:49:27 +0800

    初始化

---
+++ src/main/java/com/sky/mock/platform/MockPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sky.mock.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/mock/platform/MockPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.mock.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
